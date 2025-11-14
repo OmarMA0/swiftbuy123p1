75f11d6b9e2b66286d36b4200e0a7619b15d3424
UmarMA <omarxxx111@yahoo.com>
2025-11-14 23:20:23 +0200

    more UI fixes

--- src/views/ShoppingView.vue
+++ src/views/ShoppingView.vue
@@ -3,7 +3,7 @@ import ProductGrid from '@/components/products/ProductGrid.vue';
 </script>
 
 <template>
-    <div class="hero min-h-screen w-full text-white flex gap-10 bg-cover bg-center">
+    <div class="text-white flex">
     <ProductGrid></ProductGrid>
     </div>
 </template>
