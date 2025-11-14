1853e4b980b20f823cf0533b8f65dd27704ff998
UmarMA <omarxxx111@yahoo.com>
2025-11-14 02:28:04 +0200

    add checkout UI without functionality yet

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CartedCard from '@/components/products/CartedCard.vue';
 import { carted_products } from '@/stores/carted_products';
+import ButtonComponent from '@/components/common/ButtonComponent.vue';
 </script>
 
 <template>
@@ -8,5 +9,13 @@ import { carted_products } from '@/stores/carted_products';
     <div v-for="product in carted_products" class="flex flex-wrap">
         <CartedCard :product="product" ></CartedCard>
         </div>
+    <div class="absolute bottom-0 left-200 border-4 
+    rounded-xl p-5 m-2 w-100 justify-end h-40 bg-[#242423] border-[#f5cb5c] shadow-lg flex flex-col gap-3">
+        <p class="m-2 p-2 text-lg border-2 border-[#f5cb5c] justify-center font-bold d"> Total price = 200</p>
+        <div class="flex w-auto h-auto gap-5">
+            <ButtonComponent class="grow">Check out</ButtonComponent>
+            <ButtonComponent class="grow">continue shopping</ButtonComponent>
+        </div>
+    </div>
     </div>
 </template>
