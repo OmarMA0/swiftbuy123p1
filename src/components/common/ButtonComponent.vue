8c1712b00a57d3fe3601228ef1b0160cfe9e7f16
UmarMA <omarxxx111@yahoo.com>
2025-11-16 21:46:23 +0200

    small fix

--- src/components/common/ButtonComponent.vue
+++ src/components/common/ButtonComponent.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-<button class="bg-[#f5cb5c] text-[#080708] text-small rounded min-h-10 min-w-35 px-3 py-0.5
+<button class="bg-[#f5cb5c] text-[#080708] text-small rounded min-h-10 min-w-35 px-3 py-0.5 my-1
 font-semibold hover:bg-gray-800 cursor-pointer">
     <slot></slot> </button>
 
